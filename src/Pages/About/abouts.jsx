041fb5c8af86c4377020e9bc28a4e6ed8e57bcdb
Zanged-Jayllow <145813257+Zanged-Jayllow@users.noreply.github.com>
2025-08-04 01:15:42 -0700

    Update abouts.jsx

--- src/Pages/About/abouts.jsx
+++ src/Pages/About/abouts.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
+import Abouts_Article from './abouts-components/abouts_article'
 
 const OfficialAbouts = () => {
   return (
     <div>
         <Navbar />
         <hr></hr>
+        <Abouts_Article />
         <hr></hr>
         <Footer />
     </div>
