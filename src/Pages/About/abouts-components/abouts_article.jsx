d21f821947ea664ee45cd350d0459ca9566ae5aa
Zanged-Jayllow <145813257+Zanged-Jayllow@users.noreply.github.com>
2025-08-04 00:54:27 -0700

    Create abouts_article.jsx

---
+++ src/Pages/About/abouts-components/abouts_article.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Abouts_Article = () => {
+  return (
+    <div className='mt-5 max-w-screen-2xl mx-auto 2xl:px-20 xl:px-20 px-6 font-RobotoFlex text-center text-xl leading-normal'>
+      <p className="mb-6">
+        Brightway to Canada is...
+      </p>
+    </div>
+  )
+}
+
+export default Abouts_Article
