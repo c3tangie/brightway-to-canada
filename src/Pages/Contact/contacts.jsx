b96c04b20382e367a8a06f96db14251f4f70bd63
Zanged-Jayllow <145813257+Zanged-Jayllow@users.noreply.github.com>
2025-08-04 23:41:30 -0700

    Update contacts.jsx to include max character limits

--- src/Pages/Contact/contacts.jsx
+++ src/Pages/Contact/contacts.jsx
@@ -4,27 +4,30 @@ import Footer from '../../components/Footer'
 
 const OfficialContacts = () => {
   return (
+    <noscript>This page requires JavaScript to Run</noscript>
     <div>
         <Navbar />
         <hr></hr>
 
         <h1>Contact Us Through Our Webform</h1>
+        <p>Please do not put in personal or sensitive information in your response.</p>
+
         <form>
           <div>
               <label for="WF_name">Name:</label>
-              <input type="text" id="WF_name" name="WF_name" placeholder="Your Name"></input>
+              <input type="text" maxlength="100" id="WF_name" name="WF_name" placeholder="Your Name (Max 100 Characters)"></input>
           </div>
 
           <br></br>
 
           <div>
               <label for="WF_email">Email:</label>
-              <input type="email" id="WF_email" name="WF_email" placeholder="Your Email"></input>
+              <input type="email" maxlength="100" id="WF_email" name="WF_email" placeholder="Your Email (Max 100 Characters)"></input>
           </div>
 
           <div>
               <label for="WF_main">Main Texts:</label>
-              <input type="text" id="WF_main" name="WF_main" placeholder="Type your questions or comments here"></input>
+              <input type="text" maxlength="1024" id="WF_main" name="WF_main" placeholder="Type your questions or comments here (Max 1024 Characters)"></input>
           </div>
 
           <div>
