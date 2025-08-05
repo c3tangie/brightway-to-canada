ae03a05a3d2d940e9b91842498de21c0fa53bdff
Zanged-Jayllow <145813257+Zanged-Jayllow@users.noreply.github.com>
2025-08-05 00:41:24 -0700

    Create WIP web form submitter for Web3Forms

---
+++ src/Pages/Contact/contact-components/contact_form.jsx
@@ -0,0 +1,39 @@
+// JSX For Using Web3Forms Inside React
+// WIP DO NOT USE
+import React from "react";
+
+function WF_App() {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
+  };
+
+  return (
+      <form onSubmit={onSubmit}>
+        <input type="text" name="name"/>
+        <input type="email" name="email"/>
+        <textarea name="message"></textarea>
+        <button type="submit">Submit Form</button>
+      </form>
+  );
+}
+
+export default WF_App;
