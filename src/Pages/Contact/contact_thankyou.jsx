2247a575ed811f150c2eed69aff52909c8d01b25
Zanged-Jayllow <145813257+Zanged-Jayllow@users.noreply.github.com>
2025-08-09 00:13:56 -0700

    Create contact_thankyou.jsx

---
+++ src/Pages/Contact/contact_thankyou.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+
+const OfficialContacts = () => {
+  return (
+    <div>
+        <Navbar />
+
+        <div class="text-center">
+          <h1 class="my-3 text-3xl font-semibold text-gray-700">
+            Thank You For Your Response
+          </h1>
+        </div>
+
+        <Footer />
+    </div>
+  )
+}
+
+export default OfficialContacts
