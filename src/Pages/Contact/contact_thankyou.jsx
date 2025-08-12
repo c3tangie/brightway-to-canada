1fa0c456c2c565725e324213665581065309c693
Zanged-Jayllow <145813257+Zanged-Jayllow@users.noreply.github.com>
2025-08-12 02:48:20 -0700

    Add more descriptions to the thank you page
    maybe putting an image here, it still feels kinda empty

--- src/Pages/Contact/contact_thankyou.jsx
+++ src/Pages/Contact/contact_thankyou.jsx
@@ -11,9 +11,9 @@ const ThankyouPg = () => {
           <h1 class="my-3 text-3xl font-semibold text-gray-700">
             Thank You For Your Response
           </h1>
-          <h2 class="my-3 text-3xl font-semibold text-gray-700">
-            We are looking forward to help you with our resources 
-          </h2>
+          <p class="my-3 text-2xl font-semibold text-gray-700">
+            Your response is successfully sent to us and we will contact you on your provided email or phone number shortly. Feel free to close this page. If you wish to visit other part of this website, please click on the options in the top navigation bar or click the logo on the top left to return to our homepage. If you want to check out our social media, please click on the respective icons in the footer. 
+          </p>
         </div>
 
         <Footer />
