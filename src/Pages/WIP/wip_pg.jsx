61cf6c7550a76459a79aa839d08bd4fc7f87f909
Zanged-Jayllow <145813257+Zanged-Jayllow@users.noreply.github.com>
2025-08-12 06:37:06 -0700

    Create a page that all unfinished or abandoned links are redirected to

---
+++ src/Pages/WIP/wip_pg.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+
+const ThankyouPg = () => {
+  return (
+    <div>
+        <Navbar />
+
+        <div class="text-center">
+          <h1 class="my-3 text-3xl font-semibold text-gray-700">
+            The Content You Requested Is Still Being Worked On
+          </h1>
+          <p class="my-3 text-2xl font-semibold text-gray-700">
+            Please wait for future updates. We are sorry for the inconvinience caused. 
+          </p>
+        </div>
+
+        <Footer />
+    </div>
+  )
+}
+
+export default ThankyouPg
