42322c384c9b22a945ecb40808de0506c9286279
Zanged-Jayllow <zangjunfeng2002@163.com>
2025-12-29 21:25:57 -0800

    attempt to fix the padding issue of the wip page

--- src/Pages/WIP/wip_pg.jsx
+++ src/Pages/WIP/wip_pg.jsx
@@ -9,23 +9,24 @@ const WIPPg = ({ showNavbar = true, showFooter = true }) => {
   });
   
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
         {showNavbar && <Navbar />}
 
-        <div className="text-center">
-          <div className="h-40 sm:h-52"></div>
-           <div className="max-w-4xl mx-auto">
-            <div className="bg-black/5 border border-gray-200 shadow-lg py-0 sm:py-12 px-8 rounded-3xl">
-              <h1 className="my-3 text-xl sm:text-3xl font-bold text-gray-700">
-                The Content You Requested Is Temporarily Unavailable
-              </h1>
-              <p className="my-3 text-lg sm:text-2xl font-medium text-gray-700">
-                Please wait for future updates, we are sorry for the inconvenience. If there is an urgent request, feel free to contact us through the contact page.
-              </p>
+        <div className="flex-grow flex items-center justify-center">
+          <div className="w-full">
+            <div className="max-w-4xl mx-auto px-4 sm:px-8">
+              <div className="bg-black/5 border border-gray-200 shadow-lg py-12 px-8 rounded-3xl">
+                <h1 className="my-3 text-xl sm:text-3xl font-bold text-gray-700">
+                  The Content You Requested Is Temporarily Unavailable
+                </h1>
+                <p className="my-3 text-lg sm:text-2xl font-medium text-gray-700">
+                  Please wait for future updates, we are sorry for the inconvenience. If there is an urgent request, feel free to contact us through the contact page.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="h-40 sm:h-52"></div>  
+        
         {showFooter && <Footer />}
     </div>
   )
