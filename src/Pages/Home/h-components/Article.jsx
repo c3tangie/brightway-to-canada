e4b512b82f38c0971e48f17cff5887b1a50cb6ef
Zanged-Jayllow <zangjunfeng2002@163.com>
2026-01-03 16:44:05 -0800

    fix another thing on homepage

--- src/Pages/Home/h-components/Article.jsx
+++ src/Pages/Home/h-components/Article.jsx
@@ -235,7 +235,7 @@ const Article = () => {
               aria-label="Scroll to end"
               disabled={!showRightFade}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1 mr-1 rotate-180" viewBox="0 0 20 20" fill="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1 rotate-180" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clipRule="evenodd" />
               </svg>
               End
@@ -446,7 +446,7 @@ const Article = () => {
                 Real Life Support
               </div>
               <div className="text-xl leading-relaxed text-gray-700 mb-6">
-                We believe academic success depends on feeling supported and secure. That’s why we focus on the full student experience: school fit, learning adaptation, well-being, and ongoing communication with families.
+                We believe academic success depends on feeling supported and secure. That’s why we focus on the full student experience: learning adaptation, well-being, and ongoing communication with families.
               </div>
             </div>
           </div>
